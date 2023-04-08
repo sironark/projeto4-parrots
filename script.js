3e6e089a69c70878e7add0c2bfa7dea6fdc7f288
Gabriel Oliveira <gabriel_lopeslomeu@hotmail.com>
2023-04-08 14:11:08 -0300

    Versão com lógica de giro de carta, tempo e seleção de cartas iguais

--- script.js
+++ script.js
@@ -2,18 +2,36 @@
 let quantasCartas = 0;
 let deck=0;
 let arrayCartas=[
-    '<div class="card" onclick="rotacionar2(this)"><div class="frontFace face" ><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/back.png" alt=""></div><div class="backFace face"><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/bobrossparrot.gif" alt=""></div></div>',
-    '<div class="card" onclick="rotacionar2(this)"><div class="frontFace face" ><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/back.png" alt=""></div><div class="backFace face"><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/explodyparrot.gif" alt=""> </div></div>',
-    '<div class="card" onclick="rotacionar2(this)"><div class="frontFace face" ><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/back.png" alt=""></div><div class="backFace face"><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/fiestaparrot.gif" alt=""> </div></div>',
-    '<div class="card" onclick="rotacionar2(this)"><div class="frontFace face" ><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/back.png" alt=""></div><div class="backFace face"><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/metalparrot.gif" alt=""> </div></div>',
-    '<div class="card" onclick="rotacionar2(this)"><div class="frontFace face" ><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/back.png" alt=""></div><div class="backFace face"><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/revertitparrot.gif" alt=""></div></div>',
-    '<div class="card" onclick="rotacionar2(this)"><div class="frontFace face" ><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/back.png" alt=""></div><div class="backFace face"><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/tripletsparrot.gif" alt=""> </div></div>',
-    '<div class="card" onclick="rotacionar2(this)"><div class="frontFace face" ><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/back.png" alt=""></div><div class="backFace face"><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/unicornparrot.gif" alt=""></div></div>'
+    '<div class="card" onclick="contagem(),rotacionar1(this)"><div class="frontFace face" ><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/back.png" alt=""></div><div class="backFace face"><img class"one" src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/bobrossparrot.gif" alt=""></div></div>',
+    '<div class="card" onclick="contagem(),rotacionar1(this)"><div class="frontFace face" ><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/back.png" alt=""></div><div class="backFace face"><img class"one" src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/explodyparrot.gif" alt=""></div></div>',
+    '<div class="card" onclick="contagem(),rotacionar1(this)"><div class="frontFace face" ><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/back.png" alt=""></div><div class="backFace face"><img class"one" src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/fiestaparrot.gif" alt=""></div></div>',
+    '<div class="card" onclick="contagem(),rotacionar1(this)"><div class="frontFace face" ><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/back.png" alt=""></div><div class="backFace face"><img class"one" src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/metalparrot.gif" alt=""></div></div>',
+    '<div class="card" onclick="contagem(),rotacionar1(this)"><div class="frontFace face" ><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/back.png" alt=""></div><div class="backFace face"><img class"one" src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/revertitparrot.gif" alt=""></div></div>',
+    '<div class="card" onclick="contagem(),rotacionar1(this)"><div class="frontFace face" ><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/back.png" alt=""></div><div class="backFace face"><img class"one" src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/tripletsparrot.gif" alt=""></div></div>',
+    '<div class="card" onclick="contagem(),rotacionar1(this)"><div class="frontFace face" ><img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/back.png" alt=""></div><div class="backFace face"><img class"one" src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/unicornparrot.gif" alt=""></div></div>'
 ];
+let global1;
+let global2;
+let jogada3=""; 
+let jogada4=""; 
+contador = 0;
 
-let contador = 0;
+
 
 quantedadeDeCartas();
+
+
+
+function contagem(){
+    contador++
+    console.log(contador)
+    if(contador==2){
+        setInterval(desvirarDiferentes, 1500);
+        meuInterval = setInterval(desvirarDiferentes, 1500);
+        
+    }
+}
+
 
 
 function quantedadeDeCartas(){
@@ -48,45 +66,103 @@ function distribuirCartas(){
        return Math.random() - 0.5; 
    }
    
-   deck.innerHTML+= arrayCartasescolhidas;
+  deck.innerHTML+= arrayCartasescolhidas;
    
 
 
 }
 
-function rotacionar2(elemento){
+function rotacionar1(elemento){
+   
 
-    const rotate2 = elemento.querySelector(".frontFace");
+
+    let rotate2 = elemento.querySelector(".frontFace");
     rotate2.classList.add("rotate2");
-   
     
-   
-   //setInterval(temporizador, 2000);
+ 
 
-   const rotate1 = elemento.querySelector('.backFace');
+    let rotate1 = elemento.querySelector('.backFace');
     rotate1.classList.add('rotate1');
-    contador++
-    console.log(contador)
+    
+    
+    
+    
+    
+   console.log(contador)
 
+    if(contador == 2){
+        jogada3 =rotate1.innerHTML;
+        console.log(jogada3)
+        global2 = elemento
+
+
+       // elemento.classList.add(string);
+        
+        
+
+       
+        
+    } if(contador==1){
+        jogada4 = rotate1.innerHTML;
+        global1=elemento
+        
+        console.log(jogada4)
+        //elemento.classList.add(string3);
+        
+        
+        
+    }
+
+
+    if(jogada3 == jogada4){
+        jogada3 = "";
+        jogada4 = "";
+        contador=0;
+        console.log("cartas iguais")
+        clearInterval(meuInterval);
+        
+                
+    } 
     
 
+    
+}
+
+   
+function desvirarDiferentes(){
+    if(jogada3 != "" && jogada3!=jogada4 && jogada4!=""){
+        
+        const indice1 = global1.querySelector('.rotate1');
+        indice1.classList.remove('rotate1');
+
+        const indice11 = global1.querySelector('.rotate2');
+        indice11.classList.remove('rotate2');
+
+        
+        const indice2 = global2.querySelector('.rotate1');
+        indice2.classList.remove('rotate1');
+
+        const indice22 = global2.querySelector('.rotate2');
+        indice22.classList.remove('rotate2');
+
+      //  const indice2 = document.querySelector(string2);
+      //  const indice12 = indice2.querySelector('.rotate2');
+        
+
+   
+    
+
+        console.warn("oooooo")
+        jogada3 = "";
+        jogada4 = "";
+        contador = 0;
+        clearInterval(meuInterval);
+
+
+    }
 }
 
 
 
-function temporizador(){
-
-   
-    const i = document.querySelector('.rotate1') ;
-    i.classList.remove('rotate1');
-
-    const o = document.querySelector('.rotate2') ;
-    o.classList.remove('rotate2');
-
-    
-   
-    const meuInterval = setInterval(temporizador,1000);
-    clearInterval(meuInterval);
-}
 
 
